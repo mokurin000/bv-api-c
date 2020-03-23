a2361e4dad5728e681ee1ebfae7ce9adfc8e6b98
poly000 <1348292515@qq.com>
2020-03-23 22:46:48 +0800

    emmm

--- main.c
+++ main.c
@@ -1,20 +1,50 @@
+/*
 #include <stdio.h>
 
 char table[] = "fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF";
-char tr[256] = {0};
+long long tr[256] = {0};
 
 long long s[] = {11,10,3,8,4,6,2,9,5,7};
 long long xor = 177451812;
-long long add = 100618342136696320;
+long long add = 100618342136696320L;
 
-int main(void) {
-	for (register char i=0; i<58; i++)
-		tr[table[i]] = i;
+inline static long long llPow(long long x, unsigned int a) {
+	register unsigned int aa = a;
+	register long long s = 1;
+	for (register unsigned int i=0; i<aa; i++) {
+		s *= x;
+	}
+	return s;
 }
 
+long long dec(char x[]) {
+	register long long r = 0;
+	for (register unsigned int i=0; i<10; i++) {
+		r += llPow(tr[x[s[i]]]*58,i);
+	}
+	return (r-add)^xor;
+}
 
-//---------
+int main(void) {
+	for (register unsigned int i=0; i<58; i++) {
+		tr[table[i]] = i;
+	}
+	char p[13] = {0};
+	scanf("%s",p);
+	printf("%I64d",dec(p));
+	return 0;
+}
+
+*/ // not works
 /*
+table='fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF'
+tr={}
+for i in range(58):
+	tr[table[i]]=i
+s=[11,10,3,8,4,6,2,9,5,7]
+xor=177451812
+add=100618342136696320
+
 def dec(x):
 	r=0
 	for i in range(10):
@@ -28,10 +58,12 @@ def enc(x):
 		r[s[i]]=table[x//58**i%58]
 	return ''.join(r)
 
-print(dec('BV17x411w7KC'))
-print(dec('BV1Q541167Qg'))
-print(dec('BV1mK4y1C7Bz'))
-print(enc(170001))
-print(enc(455017605))
-print(enc(882584971))
+#print(dec('BV17x411w7KC'))
+#print(dec('BV1Q541167Qg'))
+#print(dec('BV1mK4y1C7Bz'))
+#print(enc(170001))
+#print(enc(455017605))
+#print(enc(882584971))
+print('av'+str(dec(input())))
+#print(enc(input()))
 */
