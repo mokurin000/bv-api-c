7aa1509ae3a9e5fff6a4a083aba394ef9c7d868e
poly000 <1348292515@qq.com>
2020-03-29 00:45:06 +0800

    update

--- main.c
+++ main.c
@@ -1,10 +1,43 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "bv-api/bv-api.h"
 
 #define true 1
 
-int main(void) {
+int main(int argc,char *argv[]) {
+	if (argv == 2) {
+		register const char * const p = argv[1];
+		if (!strcmp("-h",p)||!strcmp("--help",p)) {
+			fprintf(stderr,
+				"-h, --help\tshow this help.\n"
+				"[filename]\tconvert the file and write into [filename].txt .\n"
+				"\n"
+			);
+			return 0;
+		} else {
+			register size_t len = strlen(p)+1;
+			register char * const q = memcpy(malloc((len+4)*sizeof(char)), p, len);
+			if (!q) {
+				fprintf(stderr,
+					"Error while malloc memory!\n"
+					"\n"
+				);
+				return 0;
+			}
+
+			strcat(q, ".txt");
+			if (freopen(p, "r", stdin) || freopen(p, "r", stdout)) {
+				fprintf(stderr,
+					"File redirecting failed!\n"
+					"Do you have the permissions needed?\n"
+					"\n"
+				);
+				return 0;
+			}
+		}
+	}
+
 	register int stat;
 	long long num;
 	char p[13] = {0};
