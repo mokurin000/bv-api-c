f7d2e72f61d1d450cdd8fbf01659d9cee97ce86d
poly000 <1348292515@qq.com>
2020-03-29 01:40:50 +0800

    fixed a few bugs

--- _main.c
+++ _main.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "bv-api/bv-api.h"
 
 int main(int argc,char *argv[]) {
-	if (argv == 2) {
+	register FILE * fi, * fo, * fe;
+	_Bool file = 0;
+	if (argc == 2) {
+		register char * const q;
 		register const char * const p = argv[1];
-		if (!strcmp("-h",p)||!strcmp("--help",p)) {
+		if (!strcmp("-h", p) || !strcmp("--help", p)) {
 			fprintf(stderr,
 				"-h, --help\tshow this help.\n"
 				"[filename]\tconvert the file and write into [filename].txt .\n"
@@ -15,24 +19,52 @@ int main(int argc,char *argv[]) {
 			return 0;
 		} else {
 			register size_t len = strlen(p)+1;
-			register char * const q = memcpy(malloc((len+4)*sizeof(char)), p, len);
+			q = malloc((len+4)*sizeof(char));
 			if (!q) {
 				fprintf(stderr,
-					"Error while malloc memory!\n"
+					"Error while mallocing memory!\n"
 					"\n"
 				);
 				return 0;
 			}
+			memcpy(q, p, len);
 
 			strcat(q, ".txt");
-			if (freopen(p, "r", stdin) || freopen(p, "r", stdout)) {
+			if (!(fi = freopen(p, "r", stdin))) {
 				fprintf(stderr,
 					"File redirecting failed!\n"
 					"Do you have the permissions needed?\n"
 					"\n"
 				);
+				free(q);
 				return 0;
+			} else {
+				if (!(fo = freopen(q, "w", stdout))) {
+					fprintf(stderr,
+						"File redirecting failed!\n"
+						"Do you have the permissions needed?\n"
+						"\n"
+					);
+					free(q);
+					fclose(fi);
+					return 0;
+				} else {
+					if (!(fe = freopen("latest.log", "w", stderr))) {
+						fprintf(stderr,
+							"File redirecting failed!\n"
+							"Do you have the permissions needed?\n"
+							"\n"
+						);
+						free(q);
+						fclose(fi);
+						fclose(fo);
+						return 0;
+					} else {
+						file = 1;
+					}
+				}
 			}
+			free(q);
 		}
 	}
 
@@ -65,5 +97,10 @@ int main(int argc,char *argv[]) {
 			break;
 	};
 	
+	if (file) {
+		if (fclose(fi)||fclose(fo)||fclose(fe)) {
+			fprintf(stderr,"Error while closing files!\n\n");
+		}
+	}
 	return stat;
 }
