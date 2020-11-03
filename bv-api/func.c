a9c7b2508fe033cc4eaa710382244e3b680f3891
poly000 <1348292515@qq.com>
2020-11-04 03:09:52 +0800

    update

--- bv-api/func.c
+++ bv-api/func.c
@@ -4,38 +4,37 @@
 static const char table[] = "fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF";
 static int64_t tr[256] = {0};
 
-static char r[] = "BV          ";
-static int64_t s[] = {11,10,3,8,4,6,2,9,5,7};
+static char r[] = "BV1  4 1 7  ";
+static int64_t s[] = {11,10,3,8,4,6};
 static int64_t xor = INT64_C(177451812);
 static int64_t add = INT64_C(100618342136696320);
 
 void bvInitialize(void) {
-	for (register uint_fast16_t i=0; i<58; i++) {
+	for (uint_fast16_t i=0; i<58; i++) {
 		tr[(size_t)table[i]] = i;
 	}
 }
 
-inline static int64_t bvi64Pow(int64_t x, uint_fast16_t a) {
-	register uint_fast16_t aa = a;
-	register int64_t s = 1;
-	for (register uint_fast16_t i=0; i<aa; i++) {
+inline static int64_t i64Pow(int64_t x, uint_fast16_t a) {
+	int64_t s = 1;
+	for (uint_fast16_t i=0; i<a; i++) {
 		s *= x;
 	}
 	return s;
 }
 
 int64_t bvDec(char x[]) {
-	register int64_t r = 0;
-	for (register uint_fast16_t i=0; i<10; i++) {
-		r += tr[(size_t)x[s[i]]]*bvi64Pow(58,i);
+	int64_t r = 0;
+	for (uint_fast16_t i=0; i<6; i++) {
+		r += tr[(size_t)x[s[i]]]*i64Pow(58,i);
 	}
 	return (r-add)^xor;
 }
 
 char * bvEnc(int64_t x) {
 	x = (x^xor) + add;
-	for (register uint_fast16_t i=0; i<10; i++) {
-		r[s[i]] = table[x/bvi64Pow(58,i)%58];
+	for (uint_fast16_t i=0; i<6; i++) {
+		r[s[i]] = table[x/i64Pow(58,i)%58];
 	}
 	return r;
 }
